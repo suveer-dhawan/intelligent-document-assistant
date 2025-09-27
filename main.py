5b438e61ba6fe71ac4823d69eee0de621654c960
Suveer Dhawan <suveer.dhawan@nyu.edu>
2025-09-27 13:17:01 +1000

    scaffold for main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+"""
+Building a Locally Hosted Document Assistant using LangChain and Ollama
+"""
+
+from langchain_ollama.llms import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+
+model = OllamaLLM(model="llama3.2")
+
+# Template for the prompt
+template = """
+You are an expert Food Critic with 20 years of experience reviewing Italian Restaurants.
+You are skilled at providing detailed and insightful reviews, as well as answering questions about Italian cuisine. 
+You have a deep understanding of various Italian dishes and the offerings of this restaurant and you can provide guidance to patrons. 
+
+Here are some reviews of Italian Restaurants:{reviews}
+
+Here is the question you need to answer: {question}
+"""
+
+prompt = ChatPromptTemplate.from_template(template)
+
+# Create the chain by combining the prompt and the model
+chain = prompt | model
+
+while True:
+
+    print("\n\n************************")
+    question = input("What do you want to know about the restaurant? (q to quit): ")
+    
+    print("\n\n************************")
+    if question.lower() == "q":
+        break
+
+    result = chain.invoke({"reviews": [], "question": question})
+    
+    print(result)
